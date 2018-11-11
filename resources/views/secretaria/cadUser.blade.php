6e23f832fe4b5e981b50365138c913d66798d3b5
Your Name <lannysawunarafael@gmail.com>
2018-11-11 16:35:31 +0000

    Formulario do funcionario findo.

--- resources/views/secretaria/cadUser.blade.php
+++ resources/views/secretaria/cadUser.blade.php
@@ -1,103 +1,218 @@
 @extends('layouts.app')
 @section('style')
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 @endsection
 @section('content')
-<<<<<<< HEAD
-<div id="main" class="container-fluid">
- <h3 class="page-header form-group col-md-12" align="center">Ficha de registo de Funcionario</h3>
-  <form action="index.html">
+    <div id="main" class="container-fluid">
+        <h3 class="page-header form-group col-md-12" align="center">Ficha de registo de Funcionario</h3>
+        <form action="index.html">
+            <!-- Dados do Funcionario-->
+            <div id="top" class="row">
+                <!-- Seccao dos dados-->
+                <section class="form-group col-md-8">
+                    <div class="form-group col-md-4 my-labels">
+                        <label>Nome funcionario</label>
+                    </div>
+                    <div class="form-group col-md-6">
+                        <input type="text" class="form-control" id="nomeFuncionario">
+                    </div>
 
-  <!-- Dados do Funcionario-->
-  <div id="top" class="row">
-    <!-- Seccao dos dados-->
-    <section class="form-group col-md-8">
-      <div class="form-group col-md-4">
-        <label>Nome funcionario</label>
-      </div>
-      <div class="form-group col-md-8">
-        <input type="text" class="form-control" id="nomeFuncionario">
-      </div>
+                    <div class="form-group col-md-4 my-labels">
+                        <label>Data de Nascimento</label>
+                    </div>
+                    <div class="form-group col-md-6">
+                        <input type="date" name="bday" min="1960-01-01" class="form-control">
+                    </div>
 
-      <div class="form-group col-md-4">
-        <label>Data de Nascimento</label>
-      </div>
-      <div class="form-group col-md-8">
-        <input type="date" name="bday" min="1960-01-01" class="form-control">
-      </div>
+                    <div class="form-group col-md-4 my-labels">
+                        <label>Area de Formacao</label>
+                    </div>
+                    <div class="form-group col-md-6">
+                        <input type="text" class="form-control">
+                    </div>
 
-      <div class="form-group col-md-4">
-        <label>Area de Formacao</label>
-      </div>
-      <div class="form-group col-md-8">
-        <input type="text" class="form-control">
-      </div>
+                    <div class="form-group col-md-4 my-labels">
+                        <label>Sector (ocupacao)</label>
+                    </div>
+                    <div class="form-group col-md-6">
+                        <select class="form-control">
+                            <option>Nenhum</option>
+                        </select>
+                    </div>
 
-      <div class="form-group col-md-4">
-        <label>Sector (ocupacao)</label>
-      </div>
-      <div class="form-group col-md-8">
-        <select class="form-control" ><option>Nenhum</option></select>
-      </div>
+                    <div class="form-group col-md-4 my-labels">
+                        <label>Genero:</label>
+                    </div>
+                    <div class="form-group col-md-6 ">
 
-      <div class="form-group col-md-4">
-        <label>Genero:</label>
-      </div>
-      <div class="form-group col-md-8">
-        <div class="form-group col-md-4">
-          <label>Masculino<label>
-        </div>
-        <div class="form-group col-md-2">
-          <input type = "radio" id= "Masc" name= "genero" class="form-control"/>
-        </div>
-        <div class="form-group col-md-4">
-          <label>Femenino</label>
-        </div>
-        <div class="form-group col-md-2">
-          <input type = "radio" id= "Fem" name= "genero" class="form-control"/>
-        </div>
-      </div>
+                        <div class="form-group col-md-2">
+                            <input type="radio" id="Masc" name="genero" class="form-control"/>
+                        </div>
+                        <div class="form-group col-md-2">
+                            <label>Masculino</label>
+                        </div>
 
-      <div class="form-group col-md-4">
-        <label>Data de inicio</label>
-      </div>
-      <div class="form-group col-md-8">
-        <input type="date" name="bday" min="1990-01-01" class="form-control">
-      </div>
+                        <div class="form-group col-md-2">
+                            <input type="radio" id="Fem" name="genero" class="form-control"/>
+                        </div>
+                        <div class="form-group col-md-2">
+                            <label>Femenino</label>
+                        </div>
+                    </div>
 
-      <div class="form-group col-md-4">
-        <label>Data de Reforma</label>
-      </div>
-      <div class="form-group col-md-8">
-        <input type="date" name="bday" min="2000-01-01" class="form-control">
-      </div>
-    </section>
+                    <div class="form-group col-md-4 my-labels">
+                        <label>Data de inicio</label>
+                    </div>
+                    <div class="form-group col-md-6">
+                        <input type="date" name="bday" min="1990-01-01" class="form-control">
+                    </div>
 
-<!-- Seccao da foto-->
-<section class="form-group col-md-4">
-  <div class="form-group col-md-12">
-    <img  src="{{ asset('img/crianca.jpg')}}" width="400" class="img-fluid"/>
-  </div>
-  <div class="form-group col-md-6">
-    <input type = "submit" value ="Capturar" class="form-control btn btn-primary" />
-  </div>
-  <div class="form-group col-md-6">
-    <input type = "submit" value ="Carregar" class="form-control btn btn-primary" />
-  </div>
-</section>
+                    <div class="form-group col-md-4 my-labels">
+                        <label>Data de Reforma</label>
+                    </div>
+                    <div class="form-group col-md-6">
+                        <input type="date" name="bday" min="2000-01-01" class="form-control">
+                    </div>
+                </section>
 
-</div>
+                <!-- Seccao da foto-->
+                <section class="form-group col-md-4">
+                    <div class="form-group col-md-12">
+                        <img src="{{ asset('img/crianca.jpg')}}" width="400" class="img-fluid"/>
+                    </div>
+                    <div class="form-group col-md-6">
+                        <input type="submit" value="Capturar" class="form-control btn btn-primary"/>
+                    </div>
+                    <div class="form-group col-md-6">
+                        <input type="submit" value="Carregar" class="form-control btn btn-primary"/>
+                    </div>
+                </section>
 
-  <hr />
-  <div id="actions" class="row">
-    <div class="col-md-12">
-      <button type="submit" class="btn btn-primary">Salvar</button>
-      <a href="index.html" class="btn btn-default">Cancelar</a>
+            </div>
+        </form>
     </div>
-  </div>
-</form>
-</div>
-=======
 
->>>>>>> 065e9e1a8ca4cbc3cbf8f866a87c2dcddc05f75f
-@endsection
+
+
+    <div id="main" class="container-fluid">
+        <h3 class="page-header form-group col-md-12" align="center">Ficha de Endereco</h3>
+        <form action="index.html">
+            <!-- Endereco do Funcionario-->
+            <div id="top" class="row">
+                <!-- Seccao dos dados-->
+                <section class="form-group col-md-8">
+                    <div class="form-group col-md-4 my-labels">
+                        <label>Email</label>
+                    </div>
+                    <div class="form-group col-md-6">
+                        <input type="text" class="form-control" id="email-do-funcionario">
+                    </div>
+
+                    <div class="form-group col-md-4 my-labels">
+                        <label>Bairro</label>
+                    </div>
+                    <div class="form-group col-md-6">
+                        <select class="form-control">
+                            <option>Nenhum</option>
+                        </select>
+                    </div>
+
+                    <div class="form-group col-md-4 my-labels">
+                        <label>Quarteirao</label>
+                    </div>
+                    <div class="form-group col-md-6">
+                        <input type="text" class="form-control">
+                    </div>
+                </section>
+
+
+                <!-- Lado direito-->
+                <section class="form-group col-md-4">
+                    <div class="form-group col-md-4 my-labels">
+                        <label>Contacto</label>
+                    </div>
+                    <div class="form-group col-md-6">
+                        <input type="text" class="form-control" id="contacto-funcionario">
+                    </div>
+
+                    <div class="form-group col-md-4 my-labels">
+                        <label>N.da casa </label>
+                    </div>
+                    <div class="form-group col-md-6">
+                        <input type="text" class="form-control" id="numero-da-casa">
+                    </div>
+
+                    <div class="form-group col-md-4 my-labels">
+                        <label>Rua</label>
+                    </div>
+                    <div class="form-group col-md-6">
+                        <input type="text" class="form-control" id="rua-do-funcionario">
+                    </div>
+
+                </section>
+
+            </div>
+
+
+        </form>
+    </div>
+
+    <div id="main" class="container-fluid">
+        <h3 class="page-header form-group col-md-12" align="center">Acesso ao Sistema</h3>
+        <form action="index.html">
+
+            <div class="form-group col-md-4 my-labels">
+                <label>Genero:</label>
+            </div>
+            <div class="form-group col-md-6 ">
+
+                <div class="form-group col-md-2">
+                    <input type="radio" id="Masc" name="genero" class="form-control"/>
+                </div>
+                <div class="form-group col-md-2">
+                    <label>Masculino</label>
+                </div>
+
+                <div class="form-group col-md-2">
+                    <input type="radio" id="Fem" name="genero" class="form-control"/>
+                </div>
+                <div class="form-group col-md-2">
+                    <label>Femenino</label>
+                </div>
+            </div>
+            <section class="form-group col-md-8">
+                <div class="form-group col-md-4 my-labels">
+                    <label>Nome do usuario</label>
+                </div>
+                <div class="form-group col-md-6">
+                    <input type="text" class="form-control" id="usuario">
+                </div>
+            </section>
+
+            <section class="form-group col-md-4">
+                <div class="form-group col-md-4 my-labels">
+                    <label>Palavra-passe</label>
+                </div>
+                <div class="form-group col-md-6">
+                    <input type="password" class="form-control" id="palavra-passe">
+                </div>
+            </section>
+
+
+            <hr/>
+            <div id="actions" class="row pull-right" style="margin-right: 3%">
+                <div class="col-md-12">
+                    <button type="submit" class="btn btn-primary">Salvar</button>
+                    <a href="index.html" class="btn btn-default">Cancelar</a>
+                </div>
+            </div>
+        </form>
+    </div>
+            @endsection
+
+<style>
+    .my-labels {
+        margin-left: 10%;
+    }
+</style>

29b9812db28c8c32b696ff9c1dad44c3efa4e89a
Luvina97 <Carmenvictor97@gmail.com>
2018-11-16 13:30:09 +0200

    criacao de migration do formulario de visitas

---
+++ database/migrations/tabelas/2018_11_16_110101_create__visitas_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVisitasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('_visitas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('nomeVisitante');
+            $table->string('tipoDocumento');
+            $table->string('nrDoc');
+            $table->date('bday');
+            $table->string('local');
+            $table->string('assunto');
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('_visitas');
+    }
+}

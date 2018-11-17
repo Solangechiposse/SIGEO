ea6f3cce2a3aef97b0436ccd2d9767a739e8ec60
Your Name <lannysawunarafael@gmail.com>
2018-11-17 14:07:25 +0000

    controler do registo actualizado

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,19 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('password');
+            $table->string('password')->unique();
+            $table->string('quarteirao');
+            $table->string('bairro');
+            $table->string('formacao');
+            $table->string('rua');
+            $table->date('data-de-nascimento');
+            $table->date('data-de-inicio');
+            $table->date ('data-de-reforma');
+            $table-> string('ocupacao');
+            $table->string('genero');
+            $table->number('contacto')->unique();
+
+
             $table->rememberToken();
             $table->timestamps();
         });
